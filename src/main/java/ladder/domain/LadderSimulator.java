1de05554bbed2003cf6b428b7e58e69aed722cc8
jayden(김준기) <jayden.i@kakaocorp.com>
2025-04-22 21:45:24 +0900

    불필요한 세미콜론 제거

--- src/main/java/ladder/domain/LadderSimulator.java
+++ src/main/java/ladder/domain/LadderSimulator.java
@@ -41,7 +41,7 @@ public class LadderSimulator {
         int currentIndex = initialIndex;
 
         for (Line line : ladder.getLines()) {
-            currentIndex = currentIndex + line.getMove(currentIndex);;
+            currentIndex = currentIndex + line.getMove(currentIndex);
         }
 
         return new UserResult(initialIndex, users.get(initialIndex).getName(), results.get(currentIndex));
